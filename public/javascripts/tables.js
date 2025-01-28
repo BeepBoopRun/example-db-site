a9ed37e6705d5878be388af8d8bb9c4f17a86181
Ziz <>
2025-01-28 15:47:18 +0100

    back to square one

--- public/javascripts/tables.js
+++ public/javascripts/tables.js
@@ -16,38 +16,23 @@ async function getData(tableName) {
     }
 }
 
-function htmlToNodes(html) {
-    const template = document.createElement('template');
-    template.innerHTML = html;
-    return template.content.childNodes;
-}
-
 async function populateTable(tableName) {
-    console.log(this.className)
-    tableName = this.className
-    if(document.getElementsByClassName(tableName)[1].style.display == "none") {
-      document.getElementsByClassName(tableName)[1].style.display = ""
-      document.getElementsByClassName(tableName)[1].style.display = ""
-      document.getElementsByClassName(tableName)[0].style.backgroundColor = "wheat"
-      const root = document.getElementsByClassName("tables")[0].children
-      for(const c of root) {
-        c.style.order  = +c.style.order + 1 
-      }
-      document.getElementsByClassName(tableName)[1].style.order = 1
-    } else {
-      document.getElementsByClassName(tableName)[1].style.display = "none"
-      document.getElementsByClassName(tableName)[0].style.backgroundColor = ""
-      return
-    }
-    const previousData = document.getElementsByClassName(tableName)[1].getElementsByTagName("tbody")
-    if(previousData.length != 0) {
-      document.getElementsByClassName(tableName)[1].removeChild(previousData[0])
-
-    }
-    const rows =  await getData(tableName);
-    document.getElementsByClassName(tableName)[1].innerHTML += rows["list"];
-    storeQuery(rows["calledQuery"])
-    console.log(`table: ${tableName} loaded`);
+  tableName = this.className
+  const tables = document.getElementsByClassName("tables")[0]
+  const belt = document.getElementsByClassName("belt")[0]
+  if(tables.getElementsByClassName(tableName).length > 0) {
+    belt.getElementsByClassName(tableName)[0].style.backgroundColor = '';
+    tables.getElementsByClassName(tableName)[0].remove()
+    return
+  }
+  belt.getElementsByClassName(tableName)[0].style.backgroundColor = 'wheat';
+  const rows = await getData(tableName)
+  const tb = document.createElement("table")
+  tb.classList.add(tableName)
+  tb.innerHTML = rows["outHTML"]
+  tables.prepend(tb)
+  storeQuery(rows["calledQuery"])
+  console.log(`table: ${tableName} loaded`);
 }
 
 window.onload = () => {
